c6771a13755d70ce5f811e2736271caf36d17e5f
Yann BONAFONS <yann.bonafons@scmfrance.fr>
2015-10-16 17:13:10 +0200

    first commit

---
+++ ModuleBLib/ModuleBLib.h
@@ -0,0 +1,15 @@
+//
+//  ModuleBLib.h
+//  ModuleBLib
+//
+//  Created by Yann BONAFONS on 13/10/2015.
+//  Copyright © 2015 Yann BONAFONS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModuleBLib : NSObject
+
++ (void) sayHelloFromModuleB;
+
+@end
---
+++ ModuleBLib/ModuleBLib.m
@@ -0,0 +1,17 @@
+//
+//  ModuleBLib.m
+//  ModuleBLib
+//
+//  Created by Yann BONAFONS on 13/10/2015.
+//  Copyright © 2015 Yann BONAFONS. All rights reserved.
+//
+
+#import "ModuleBLib.h"
+
+@implementation ModuleBLib
+
++ (void) sayHelloFromModuleB{
+    NSLog(@"Hello from module modified in the project!");
+}
+
+@end
